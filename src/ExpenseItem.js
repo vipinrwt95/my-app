97d808faed0f3691d3cc511bc2d1915c24732dc4
Vipin <vipinrwt9@gmail.com>
2022-11-12 06:35:37 +0000

    delete without proper functionality

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,23 +1,23 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
+import Card from '../UI/Card';
 
-function ExpenseItem()
-{ const expensedate=new Date(2021,2,28);
-  const expenseTitle='Car purchase';
-  const expenseAmount=500;
-  const expenseLocation='Delhi'; 
-  return (
-   <div className="expense-item">
-    <div>{expensedate.toISOString()}</div>
-    <div className="expense-item__description">
-     <h2>{expenseTitle}</h2>
-     <div>{expenseLocation}</div>
-     <div className="expense-item__price">${expenseAmount}</div>
-     
-     </div>
-  </div>
+function ExpenseItem(props)
+{  const removeItemHandler=()=>{
+ 
+   let parent=document.querySelector('.expenses')
+   let childtoberemoved=document.querySelector('.expense-item');
+   parent.removeChild(childtoberemoved); 
 
-  );
+}
+ return (
+     <Card className='expense-item'> 
+     <ExpenseDate date={props.date}></ExpenseDate>
+     <ExpenseDetails title={props.title} amount={props.amount} ></ExpenseDetails>
+     <button onClick={removeItemHandler}>DELETE EXPENSE</button>
+     </Card>
+);
 }
 
-
-export default ExpenseItem;
+export default ExpenseItem; 
