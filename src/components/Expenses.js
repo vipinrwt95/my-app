73223d9d2c57ddece1115cea116e2c588c652603
Vipin <vipinrwt9@gmail.com>
2022-11-13 16:41:21 +0000

    adding list functionality

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
@@ -12,22 +13,13 @@ const Expenses = (props) => {
   };
  const filteredExpenses=props.items.filter((expense)=>{
    return expense.date.getFullYear().toString()===filteredYear;
- })
- 
+ });
 
-  return (
+return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {
-        filteredExpenses.map((expense)=>
-        <ExpenseItem 
-          key={expense.id}
-         title={expense.title}
-         amount={expense.amount} 
-         date={expense.date}/>
-         )
-         }
+        <ExpensesList items={filteredExpenses} />
        </Card>
     </div>
   );
