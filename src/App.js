be9af0d41cf4945fec3ef49e3c227db7d51f305b
Vipin <vipinrwt9@gmail.com>
2022-11-13 14:49:05 +0000

    Update App.js
    making app dynamic

--- src/App.js
+++ src/App.js
@@ -1,61 +1,54 @@
-import ExpenseItem from "./components/Expenses/ExpenseItem";
+import React,{useState} from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import './components/Expenses/ExpenseItem.css';
+
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    location: 'LA'
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12),
+    location:'UK' },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    location:'NZ'
+
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location:'NYC'
+  },
+];
+
 
 const App=()=> {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: 'LA'
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12),
-      location:'UK' },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location:'NZ'
+  const[expenses,setExpenses]=useState(DUMMY_EXPENSES);
 
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location:'NYC'
-    },
-  ];
+
+
+  const addExpenseHandler=(expense)=>
+  { console.log(expense);
+   setExpenses((prevExpenses)=>{
+    return [expense,...prevExpenses];
+   })
+  };
   
   return (
-    <div>
-      <h1>EXPENSE TRACKER</h1>
-       <ExpenseItem
-         date={expenses[0].date}
-         title={expenses[0].title}
-         amount={expenses[0].amount}
-         location={expenses[0].location}>
-      </ExpenseItem>
-      <ExpenseItem
-         date={expenses[1].date}
-         title={expenses[1].title}
-         amount={expenses[1].amount}
-         location={expenses[1].location}>
-      </ExpenseItem>
-      <ExpenseItem
-         date={expenses[2].date}
-         title={expenses[2].title}
-         amount={expenses[2].amount}
-         location={expenses[2].location}>
-      </ExpenseItem>
-      <ExpenseItem
-         date={expenses[3].date}
-         title={expenses[3].title}
-         amount={expenses[3].amount}
-         location={expenses[3].location}>
-      </ExpenseItem> 
-        
+   <div>
+    <NewExpense onAddExpense={addExpenseHandler}/>
+    <Expenses items={expenses}/>
+    
     </div>
   );
 }
