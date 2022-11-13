cd0e0f2e47e1b96795c8b5d0fdd9b54556d2a807
Vipin <vipinrwt9@gmail.com>
2022-11-13 13:17:56 +0000

    Update ExpenseForm.js

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,14 +1,15 @@
 import React,{ useState } from'react';
 import './ExpenseForm.css';
 
-const ExpenseForm=()=>
+const ExpenseForm=(props)=>
 {  
-  const [enteredtitle,setenteredTitle]=useState();
-  const [enteredamount,setenteredAmount]=useState();
-  const [entereddate,setenteredDate]=useState();
+  const [enteredtitle,setenteredTitle]=useState('');
+  const [enteredamount,setenteredAmount]=useState('');
+  const [entereddate,setenteredDate]=useState('');
 
   const titlechangeHandler=(event)=>{
    setenteredTitle(event.target.value)
+
   }
   
   const amountchangeHandler=(event)=>
@@ -29,7 +30,10 @@ const ExpenseForm=()=>
 
     }
 
-    console.log(ExpenseData);
+    props.onSaveExpenseData(ExpenseData);
+    setenteredTitle('');
+    setenteredAmount('');
+    setenteredDate('');
 
   }
 
@@ -38,15 +42,17 @@ const ExpenseForm=()=>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
             <label>Title</label>
-            <input type='text' onChange={titlechangeHandler} />
+            <input type='text'
+            value={enteredtitle} 
+            onChange={titlechangeHandler}/>
         </div>
         <div className='new-expense__control'>
             <label>Amount</label>
-            <input type='number' onChange={amountchangeHandler} min="0.01" step="0.01" />
+            <input type='number'value={enteredamount} onChange={amountchangeHandler} min="0.01" step="0.01" />
         </div>
         <div className='new-expense__control'>
             <label>Date</label>
-            <input type='date' min="2019-01-01" max="2022-12-31" onChange={datechangeHandler} />
+            <input type='date' min="2019-01-01" max="2022-12-31"value={entereddate} onChange={datechangeHandler} />
         </div>
         <div className='new-expense__actions'><button type='submit' >Add Expense</button></div>
         </div>  
